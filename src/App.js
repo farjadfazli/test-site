7dfdd3922e029c1f254f7a350522e1c6c957e06d
farjadfazli <farjadfazli@gmail.com>
2019-11-04 20:51:57 -0500

    changed welcome message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to My Example Site!
+          Welcome to Farjad's Example Site!
         </p>
         <a
           className="App-link"
